ad83f830b95f4356d5e843e45ca972877a07d927
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-09 21:37:58 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.Clear();
+
+double numero1 = 0;
+double numero2 = 0;
+double numero3 = 0;
+
+Console.WriteLine("--- Exercicio Maior de Tres ---");
+
+Console.Write("Digite o primeiro valor: ");
+numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo valor: ");
+numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o terceiro valor: ");
+numero3 = Convert.ToDouble(Console.ReadLine());
+
+
+if (numero1 > numero2 && numero1 > numero3)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Esse é o maior numero: {numero1}");
+}
+
+else if (numero2 > numero1 && numero2 > numero3)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Esse é o maior numero: {numero2}");
+}
+else 
+{
+    Console.WriteLine();
+    Console.WriteLine($"Esse é o maior numero: {numero3}");
+}
